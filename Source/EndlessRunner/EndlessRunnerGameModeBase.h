c57f5c299282f08c342db0ce4803b61dc49a9865
Faisal-02 <149487419+Faisal-02@users.noreply.github.com>
2024-01-15 22:33:35 +0300

    Initial commit

---
+++ Source/EndlessRunner/EndlessRunnerGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EndlessRunnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENDLESSRUNNER_API AEndlessRunnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
